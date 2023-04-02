19fd7057bc12a9232be27f228d01be7b2d4cbcdb
Leonardo Reyes <reyesleonardo44@yahoo.com>
2023-04-01 23:59:44 -0700

    Arithmetic lab complete

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 20;
+let num4 = 4;
+let mod = num4 % num3;
+
+let randomNumSet = (1, 8, 4, 20);
+let max = Math.max(randomNumSet);
